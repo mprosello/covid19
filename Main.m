d79b09239de86bf6421344c6825598df920e3866
Martin Perello Rosello <marpero3@ific.uv.es>
2020-04-03 20:28:37 +0200

    Daily Update

--- Main.m
+++ Main.m
@@ -7,6 +7,7 @@ file_of_death = strcat(path, name_file_1, 'deaths', name_file_2, extension);
 file_of_recovered = strcat(path, name_file_1, 'recovered', name_file_2, extension);
 fig = strcat(name_file_1, 'spain.png');
 fig2 = strcat(name_file_1, 'global.png');
+fig3 = strcat(name_file_1, 'daily_spain.png');
 
 DataConfirmed = importdata(file_of_confirmed,',',1);
 DataDeath = importdata(file_of_death,',',1);
@@ -28,6 +29,7 @@ figure
 semilogy(Date,ConfirmedSpain,'*b',Date,RecoveredSpain,'*g',Date,DeathSpain,'*r')
 h = legend ("Confirmats","Recuperats","Morts","northwest");
 legend (h, "location", "northwest");
+text (pi, 1000, ctime (time ()));
 xlabel('Dia des de que van començar a contar els xinesos')
 ylabel('Nombre de casos en Espanya')
 saveas(gcf, fig)
@@ -52,6 +54,35 @@ semilogy(DateGlobal,ConfirmedGlobal,'*b',DateGlobal,RecoveredGlobal,'*g',DateGlo
 %semilogy(DateGlobal,ConfirmedGlobal,'*b')
 h2 = legend ("Confirmats","Recuperats","Morts","northwest");
 legend (h2, "location", "northwest");
+text (pi, 1000, ctime (time ()));
 xlabel('Dia des de que van començar a contar els xinesos')
 ylabel('Nombre de casos global')
 saveas(gcf, fig2)
+
+ConfirmedSpainAux = ConfirmedSpain;
+ConfirmedSpainAux(1) = [];
+ConfirmedSpain(length(ConfirmedSpain)) = [];
+DailyConfirmedSpain = ConfirmedSpainAux - ConfirmedSpain
+
+DeathSpainAux = DeathSpain;
+DeathSpainAux(1) = [];
+DeathSpain(length(DeathSpain)) = [];
+DailyDeathSpain = DeathSpainAux - DeathSpain;
+
+RecoveredSpainAux = RecoveredSpain;
+RecoveredSpainAux(1) = [];
+RecoveredSpain(length(RecoveredSpain)) = [];
+DailyRecoveredSpain = RecoveredSpainAux - RecoveredSpain;
+
+DateDaily = 1:length(DailyRecoveredSpain);
+
+figure
+plot(DateDaily,DailyConfirmedSpain,'-b',"linewidth",2,DateDaily,DailyRecoveredSpain,'-g',"linewidth",2,DateDaily,DailyDeathSpain,'-r',"linewidth",2)
+h3 = legend ("Confirmats","Recuperats","Morts","northwest");
+legend (h3, "location", "northwest");
+text (pi, 1000, ctime (time ()));
+xlabel('Dia des de que van començar a contar els xinesos')
+ylabel('Nombre de casos diaris Espanya')
+saveas(gcf, fig3)
+
+
